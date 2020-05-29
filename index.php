29619bb2c97cb6bca88715f135363b8559021503
sarthakAWM <47440312+sarthakAWM@users.noreply.github.com>
2020-05-29 10:59:01 +0530

    first

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Sathak_Demo_Site</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+		<div class="page-wrap">
+
+			<!-- Nav -->
+				<nav id="nav">
+					<ul>
+						<li><a href="index.html" class="active"><span class="icon fa-home"></span></a></li>
+						<li><a><span class="icon fa-camera-retro"></span></a></li>
+						<li><a><span class="icon fa-file-text-o"></span></a></li>
+					</ul>
+				</nav>
+
+			<!-- Main -->
+				<section id="main">
+
+					<!-- Banner -->
+						<section id="banner">
+							<div class="inner">
+								<h1>WELCOME TO THE DEVOPS WORLD SARTHAK JOSHI</h1>
+							</div>
+						</section>
+
+				</section>
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+		<?php phpinfo();
+		?>
+	</body>
+</html>
